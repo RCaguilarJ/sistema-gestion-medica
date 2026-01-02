b6fdd04e75ebc1d8a40d3dc80f11aef72963f3f2
Roberto Carlos Aguilar Jiménez <roberto.aguilar3953@alumnos.udg.mx>
2026-01-02 16:25:52 -0600

    ajustes para producción

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,36 @@
 import axios from "axios";
 
-// ---------------------------------------------------------------------------
-// CONFIGURACIÓN DE URL BASE
-// Usamos la dirección absoluta del backend porque está en un subdominio distinto ('back')
-// Esto asegura que tanto en desarrollo como en producción se apunte al servidor correcto.
-// ---------------------------------------------------------------------------
-const baseURL = 'https://back.diabetesjalisco.org/api';
+
+// CONFIGURACIÓN DE LA URL BASE (Backend)
+// -----------------------------------------------------------------------------
+//Usar esta línea para producción
+// const baseURL = 'https://back.diabetesjalisco.org/api';
+
+// Esta lína es para trabajar en puebas locales 
+const baseURL = "http://localhost:4000/api";
+
+// -----------------------------------------------------------------------------
 
 const api = axios.create({
   baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Interceptor para agregar el Token de sesión automáticamente a cada petición
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
 export default api;
